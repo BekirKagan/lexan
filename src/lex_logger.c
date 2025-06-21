d14bc583060baf4800baaa8fd48ce91fcf2da799
BekirKagan <kagankaraahmetoglu@hotmail.com>
2025-06-21 14:04:00 +0300

    Changed license to MIT.

--- src/lex_logger.c
+++ src/lex_logger.c
@@ -1,19 +1,6 @@
 /*
- *   lex_logger.c - A lightweight C logging library with color-coded output and simple macros.
- *   Copyright (C) 2025 Bekir Kağan Karaahmetoğlu <kagankaraahmetoglu@hotmail.com>
- *
- *   This program is free software: you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version.
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * SPDX-License-Identifier: MIT
+ * Copyright (c) 2025 Bekir Kağan Karaahmetoğlu <kagankaraahmetoglu@hotmail.com>
  */
 
 #include <lexan/lex_logger.h>
